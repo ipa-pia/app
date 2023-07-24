73644088a5517e64eda9cfd45dee991bff2dd20b
leoking <present150608@sina.com>
2023-07-24 13:23:42 +0800

    feat: vuepress

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,15 @@
+/*
+ * @Author: leoking
+ * @Date: 2023-07-24 13:08:00
+ * @LastEditTime: 2023-07-24 13:20:22
+ * @LastEditors: leoking
+ * @Description: 
+ */
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+    lang: 'zh-CN',
+    title: 'IPA',
+    description: 'Intelligent Personal Assistance',
+    base: "/",
+})
