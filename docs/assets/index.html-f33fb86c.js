84f3074d8fdc409795cade5add734e102dfa9f8a
leoking <present150608@sina.com>
2023-07-25 12:51:39 +0800

    build

---
+++ docs/assets/index.html-f33fb86c.js
@@ -0,0 +1,1 @@
+const t=JSON.parse('{"key":"v-2da6409b","path":"/shortcuts/","title":"快捷键","lang":"zh-CN","frontmatter":{"lang":"zh-CN","description":"系统快捷键、用户自定义插件快捷键"},"headers":[],"git":{"updatedTime":1690259647000,"contributors":[{"name":"leoking","email":"present150608@sina.com","commits":1}]},"filePathRelative":"shortcuts/index.md"}');export{t as data};
